b09fa6c9bb26c42bc2ff2205d74af4f45510ba0b
Matthew Gershkovich <g.matthew.2004@gmail.com>
2022-05-07 23:47:12 -0400

    Added files

---
+++ Chrome Extension/content.js
@@ -0,0 +1,2 @@
+document.querySelector("body").innerHTML = "<h1 style='color: white !important; text-align: center !important;'>Go back to work<h1>";
+document.querySelector("body").style.backgroundColor = "#000000";
