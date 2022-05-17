2a0cb9615c73d884ab19d8834e5584daa067b98a
Matthew Gershkovich <g.matthew.2004@gmail.com>
2022-05-16 20:15:33 -0400

    changed logo and added blocked page

--- content.js
+++
@@ -1,2 +0,0 @@
-//document.querySelector("body").innerHTML = "<h1 style='color: white !important; text-align: center !important;'>Go back to work<h1>";
-//document.querySelector("body").style.backgroundColor = "#000000";
