a546706265babc9b3780c978f803f5a64dbe88f1
Matthew Gershkovich <g.matthew.2004@gmail.com>
2022-06-07 11:18:03 -0400

    added dropdown

---
+++ popupForm.js
@@ -0,0 +1,7 @@
+function showText(){
+    if(select.value==1){
+    document.getElementById('hidden_div').style.display = "block";
+    } else{
+    document.getElementById('hidden_div').style.display = "none";
+    }
+} 
