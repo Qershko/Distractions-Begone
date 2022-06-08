7384dc46123f8316968883eff05126795bfd3e07
Matthew Gershkovich <g.matthew.2004@gmail.com>
2022-06-07 22:51:17 -0400

    Got input and added table of inputs

---
+++ popup.js
@@ -0,0 +1,91 @@
+let submitButton = document.getElementById('submit-button');
+let siteList = [];
+let siteListEl = document.getElementById('site-list');
+let siteID = 0;
+
+function toggleTimer() {
+    let style = document.getElementById('action').value == 1 ? 'block' : 'none';
+    document.getElementById('hidden_div').style.display = style;
+}
+
+function showDebug(content) {
+    document.querySelector('#debug').innerHTML = content;
+}
+
+
+function addSiteEntry(siteEntry) {
+    siteListEl.innerHTML += `<div class="site-entry" ><span>${siteEntry.site}</span><span>${siteEntry.time ? "Reminder every " + siteEntry.time + " mins" : "Blocked"}</span><span><button id="remove-${siteEntry.id}">X</button></span></div>`;
+    showDebug(dump(siteList))
+    document.getElementById(`remove-${siteEntry.id}`).addEventListener('click', function () {
+        removeSite(siteEntry.id)
+    });
+
+}
+
+function dump(obj) {
+    var out = '';
+    for (var i in obj) {
+        out += i + ": " + (typeof obj[i] === 'object' ? dump(obj[i]) : obj[i]) + "\n";
+    }
+
+    return out;
+}
+
+function removeSite(id) {
+
+    siteList = siteList.filter((s) => s.id != id);
+    showDebug("rmeoveSite: " + dump(siteList))
+
+    siteListEl.innerHTML = "";
+    siteList.forEach((siteEntry) => {
+        addSiteEntry(siteEntry)
+    })
+
+}
+
+document.getElementById('action').addEventListener('change', function () {
+    toggleTimer();
+
+    document.querySelector("#timer").value = "";
+});
+
+submitButton.addEventListener('click', function () {
+    try {
+
+        let siteEntry = {};
+        let site = document.querySelector("#website");
+        let blockType = document.querySelector("#timer");
+
+        siteEntry = { id: siteID, site: site.value, time: blockType.value != "" ? blockType.value : undefined };
+        siteList.push(siteEntry);
+
+        addSiteEntry(siteEntry);
+
+        site.value = "";
+        blockType.value = "";
+        document.getElementById('action').value = "0"
+        toggleTimer();
+        siteID++;
+
+
+    } catch (e) {
+        showDebug(e);
+    }
+})
+
+// chrome.storage.sync.set({ test: 'Matthew Boy' }, function () {
+//     //console.log('Value is set to ' + value);
+// });
+
+
+// chrome.storage.sync.get(['test'], function (result) {
+//     test = result.key;
+// });
+//chrome.storage.sync.set({ test: "Mattyusha" });
+//test = sessionStorage.setItem('test', "Mattyusha")
+//console.log(test);
+
+// chrome.storage.sync.get('test', function (data) {
+//     document.querySelector('#debug').innerHTML = data.mytext;
+// });
+
--- popupForm.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('action').addEventListener('change', function () {
-    var style = this.value == 1 ? 'block' : 'none';
-    document.getElementById('hidden_div').style.display = style;
-});
