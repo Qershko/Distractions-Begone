f9d299ef2856dd270e82c093ef63c4cc16c892c8
Matthew Gershkovich <g.matthew.2004@gmail.com>
2022-06-23 13:57:05 -0400

    Save feature working

--- popup.js
+++ popup.js
@@ -1,90 +1,111 @@
+/*
 let submitButton = document.getElementById('submit-button');
 let siteList = [];
 let siteListEl = document.getElementById('site-list');
 let siteID = 0;
 
 function toggleTimer() {
-    let style = document.getElementById('action').value == 1 ? 'block' : 'none';
-    document.getElementById('hidden_div').style.display = style;
+   let style = document.getElementById('action').value == 1 ? 'block' : 'none';
+   document.getElementById('hidden_div').style.display = style;
 }
 
 function addSiteEntry(siteEntry) {
-    siteListEl.innerHTML += `<div class="site-entry" ><span>${siteEntry.site}</span><span>${siteEntry.time ? "Reminder every " + siteEntry.time + " mins" : "Blocked"}</span><span><button id="remove-${siteEntry.id}">X</button></span></div>`;
-    showDebug(dump(siteList))
-    document.getElementById(`remove-${siteEntry.id}`).addEventListener('click', function () {
-        removeSite(siteEntry.id)
-    });
+   siteListEl.innerHTML += `<div class="site-entry" ><span>${siteEntry.site}</span><span>${siteEntry.time ? "Reminder every " + siteEntry.time + " mins" : "Blocked"}</span><span><button id="remove-${siteEntry.id}">X</button></span></div>`;
+   showDebug(dump(siteList))
+   document.getElementById(`remove-${siteEntry.id}`).addEventListener('click', function () {
+       removeSite(siteEntry.id)
+   });
 
 }
 
 function dump(obj) {
-    var out = '';
-    for (var i in obj) {
-        out += i + ": " + (typeof obj[i] === 'object' ? dump(obj[i]) : obj[i]) + "\n";
-    }
+   var out = '';
+   for (var i in obj) {
+       out += i + ": " + (typeof obj[i] === 'object' ? dump(obj[i]) : obj[i]) + "\n";
+   }
 
-    return out;
+   return out;
 }
 
 function removeSite(id) {
 
-    siteList = siteList.filter((s) => s.id != id);
-    showDebug("removeSite: " + dump(siteList))
+   siteList = siteList.filter((s) => s.id != id);
+   showDebug("removeSite: " + dump(siteList))
 
-    siteListEl.innerHTML = "";
-    siteList.forEach((siteEntry) => {
-        addSiteEntry(siteEntry)
-    })
+   siteListEl.innerHTML = "";
+   siteList.forEach((siteEntry) => {
+       addSiteEntry(siteEntry)
+   })
 
 }
 
 document.getElementById('action').addEventListener('change', function () {
-    toggleTimer();
+   toggleTimer();
 
-    document.querySelector("#timer").value = "";
+   document.querySelector("#timer").value = "";
 });
 
 submitButton.addEventListener('click', function () {
-    try {
+   try {
 
-        let siteEntry = {};
-        let site = document.querySelector("#website");
-        let blockType = document.querySelector("#timer");
+       let siteEntry = {};
+       let site = document.querySelector("#website");
+       let blockType = document.querySelector("#timer");
 
-        siteEntry = { id: siteID, site: site.value, time: blockType.value != "" ? blockType.value : undefined };
-        siteList.push(siteEntry);
+       siteEntry = { id: siteID, site: site.value, time: blockType.value != "" ? blockType.value : undefined };
+       siteList.push(siteEntry);
 
-        addSiteEntry(siteEntry);
+       addSiteEntry(siteEntry);
 
-        site.value = "";
-        blockType.value = "";
-        document.getElementById('action').value = "0"
-        toggleTimer();
-        siteID++;
+       site.value = "";
+       blockType.value = "";
+       document.getElementById('action').value = "0"
+       toggleTimer();
+       siteID++;
 
 
-    } catch (e) {
-        //showDebug(e);
-    }
+   } catch (e) {
+       showDebug(e);
+   }
 })
 
-/*
 function showDebug(content) {
-    document.querySelector('#debug').innerHTML = content;
+   document.querySelector('#debug').innerHTML = content;
 }
 */
+let debug = document.querySelector('#debug');
 
-window.onload = function(){
-    document.getElementById('submit-button').onclick = function() {
+window.onload = function () {
+    document.getElementById('submit-button').onclick = function () {
         var value = document.getElementById('website').value;
-        //alert(value);
+        const inputField = document.getElementById('website');
 
-        chrome.storage.sync.set({'myLine': value}, function() {
-            alert(value);
+        inputField.placeholder="youtube.com";
+
+        chrome.storage.sync.get('db_blocked_sites', (d) => {
+            let sites = []
+            if ('db_blocked_sites' in d) {
+                sites = d['db_blocked_sites']
+            }
+            sites.push({ site: value });
+            chrome.storage.sync.set({ 'db_blocked_sites': sites }, function () {
+            });
+
         });
     }
 
-    chrome.storage.sync.get(['myLine'], function() {
-        console.log('Value currently is ' + result.key);
-    });
+    document.getElementById('get').onclick = function () {
+        chrome.storage.sync.get('db_blocked_sites', function (data) {
+            if ('db_blocked_sites' in data) {
+                debug.innerHTML = "";
+                data['db_blocked_sites'].map((w) => { debug.innerHTML += w.site })
+            }
+        });
+    }
+
+    document.getElementById('clear').onclick = function () {
+        chrome.storage.sync.clear(function () {
+            alert("All clear");
+        })
+    }
 }
